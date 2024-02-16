0e677e47008a372ac89b69d10f28af37b1e965ce
Knut Rand <knutdrand@gmail.com>
2024-02-16 17:07:06 +0100

    temperature included

--- probabilistic_machine_learning/cases/diff_encoded_mosquito.py
+++ probabilistic_machine_learning/cases/diff_encoded_mosquito.py
@@ -10,7 +10,7 @@ import scipy.stats as s_stats
 from jax.scipy import stats
 from jax.scipy.special import expit, logit
 from probabilistic_machine_learning.adaptors.jax_nuts import sample as nuts_sample
-from functools import wraps
+from functools import wraps, partial
 
 
 def diff_encoded_model(transition, init_state, observation_dist, diff_dist):
@@ -18,6 +18,11 @@ def diff_encoded_model(transition, init_state, observation_dist, diff_dist):
         observation_key, transition_key = jax.random.split(key)
         state = jnp.array(init_state)
         dist = diff_dist(state, params, exogenous)
+
+        def exogenous_sample(state, values):
+            key, exog = values
+            return transition(state, diff_dist(state, params, exog).sample(key))
+
         if isinstance(dist, tuple):
             N = len(dist)
             sample_transition = lambda state, t_key: transition(state, jnp.array([d.sample(k) for d, k in
@@ -25,7 +30,14 @@ def diff_encoded_model(transition, init_state, observation_dist, diff_dist):
                                                                                       jax.random.split(t_key, N))]))
         else:
             sample_transition = lambda state, key: transition(state, diff_dist(state, params).sample(key))
-        state = jax.lax.scan(sample_transition, state, jax.random.split(transition_key, T - 1))[1]
+
+        values = jax.random.split(transition_key, T - 1)
+
+        if exogenous is not None:
+            sample_transition = exogenous_sample
+            values = (values, exogenous)
+
+        state = jax.lax.scan(sample_transition, state, values)[1]
         return observation_dist(state).sample(observation_key)
 
     def recontstruct_state(logits_array):
@@ -35,16 +47,16 @@ def diff_encoded_model(transition, init_state, observation_dist, diff_dist):
         diffs = P['logits_array']
         state = recontstruct_state(diffs)
 
-        d_dist = diff_dist(state[:-1], P, exogenous)
+        d_dist = diff_dist(state[:-1], P, exogenous[1:])
         if isinstance(d_dist, tuple):
             state_pdf = sum(dist.log_prob(diff[1:]).sum() for dist, diff in zip(d_dist, diffs.T))
             init_pdf = sum(dist.log_prob(diff[0]) for dist, diff in zip(diff_dist(init_state, P), diffs.T))
         else:
             state_pdf = d_dist.log_prob(diffs[1:]).sum()
-            init_pdf = diff_dist(init_state, P).log_prob(diffs[0]).sum()
+            init_pdf = diff_dist(init_state, P, exogenous[0]).log_prob(diffs[0]).sum()
         return state_pdf + observation_dist(state, P).log_prob(observed).sum() + init_pdf
 
-    return sampler, lambda observed: (lambda kwargs: log_prob(observed, kwargs)), recontstruct_state
+    return sampler, lambda observed, exogenous=None: (lambda kwargs: log_prob(observed, kwargs, exogenous)), recontstruct_state
 
 
 def loc_scale_warp(dist):
@@ -82,7 +94,7 @@ Logisitic = Distribution(loc_scale_warp(jax.random.logistic), stats.logistic.log
 
 
 def refactor_model(param_dict):
-    def diff_dist(state, P=param_dict,exogenous=None):
+    def diff_dist(state, P=param_dict, exogenous=None):
         P = {k: P[k] if k in P else param_dict[k] for k in param_dict}
         loc = [P['beta'] + logit(state[..., 2]), P['lo_gamma'], P['lo_a'], P['lo_mu']]
         return tuple(Logisitic(l, jnp.exp(P['logscale'])) for l in loc)
@@ -105,7 +117,7 @@ def simple_model(reporting_rate=10000, logscale=jnp.log(0.1)):
     lo_gamma, lo_a, lo_mu = (logit(p) for p in (0.1, 0.1, 0.05))
 
     def diff_dist(state, P, exogenous=None):
-        return Logisitic(loc=P['beta'] + jnp.log(state[..., 2]), scale=jnp.exp(logscale))
+        return Logisitic(loc=P['beta']*exogenous + jnp.log(state[..., 2]), scale=jnp.exp(logscale))
 
     def observation_dist(state, P=None):
         return Poisson(state[..., 2] * reporting_rate)
@@ -147,16 +159,16 @@ def check_big_model():
     plt.show()
 
 
-def model_evaluation_plot(sample_func, real_params, sampled_params):
+def model_evaluation_plot(sample_func, real_params, sampled_params,temperature):
     reporting_rate = 10000
-    T = 100
+    T = len(temperature)+1
     n_samples = len(sampled_params[list(real_params)[0]])
     for i in range(1, n_samples, n_samples // 10):
         new_observed = sample_func(T, jax.random.PRNGKey(i),
-                                   {param_name: sampled_params[param_name][i] for param_name in real_params})
+                                   {param_name: sampled_params[param_name][i] for param_name in real_params}, temperature)
         plt.plot(new_observed / reporting_rate, label='new_observed{}'.format(i), color='grey')
     for key in [200, 2000, 50, 99999]:
-        alt_observed = sample_func(T, jax.random.PRNGKey(key), real_params)
+        alt_observed = sample_func(T, jax.random.PRNGKey(key), real_params, temperature)
         plt.plot(alt_observed / reporting_rate, label='alt_observed{}'.format(key), color='blue')
     plt.show()
 
@@ -170,16 +182,19 @@ def debug_logprob(log_prob, sampled_parameters):
 def check_small_model():
     real_params = {'beta': 0.5}
     sample, log_prob, reconstruct_state = simple_model()
-    T = 10
-    observed = sample(T, jax.random.PRNGKey(100), real_params)
+    T = 100
+    temperature = np.random.normal(1, 1, T-1)
+    observed = sample(T, jax.random.PRNGKey(100), real_params, temperature)
     init_diffs = np.random.normal(0, 1, (T - 1))
     init_dict = {'logits_array': init_diffs} | {'beta': 0.0}
-    print(log_prob(observed)(init_dict))
-    print(jax.grad(log_prob(observed))(init_dict))
-    samples = nuts_sample(log_prob(observed), jax.random.PRNGKey(0),
+    lp = log_prob(observed, temperature)
+    print(lp(init_dict))
+    print(jax.grad(lp)(init_dict))
+
+    samples = nuts_sample(lp, jax.random.PRNGKey(0),
                           init_dict, 200, 100)
-    debug_logprob(log_prob(observed), samples)
-    model_evaluation_plot(sample, real_params, samples)
+    debug_logprob(lp, samples)
+    model_evaluation_plot(sample, real_params, samples, temperature)
     plt.plot(samples['beta'], label='beta');
     plt.show()
     plt.plot(observed / 10000, label='observed')
@@ -191,5 +206,5 @@ def check_small_model():
 
 
 if __name__ == '__main__':
-    # check_small_model()
-    check_big_model()
+    check_small_model()
+    #check_big_model()
