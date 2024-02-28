7fda8618c3795efc6520e9400ac8d4eb4f88ef30
Knut Rand <knutdrand@gmail.com>
2024-02-28 09:01:22 +0100

    feature

--- tests/test_hybrid_model.py
+++ tests/test_hybrid_model.py
@@ -16,3 +16,16 @@ def test_decoupled_scan(simple_diffs):
     scanned = decoupled_scan(*simple_diffs)
     truth = [10, 11, 13, 16, 20, 105, 111, 118, 126, 135]
     assert_array_equal(scanned, np.array(truth)[:, None])
+
+@pytest.fixture
+def simple_diffs_2d():
+    diffs = np.array([np.arange(4), np.zeros(4)]).T
+    assert diffs.shape == (4, 2)
+    states = np.array([[10, 1], [100, 10.]])
+    return diffs, states
+
+def test_2d_scan(simple_diffs_2d):
+    scanned = decoupled_scan(*simple_diffs_2d)
+    truth = [[10, 1], [11, 1], [102, 10], [105, 10]]
+    assert_array_equal(scanned, np.array(truth))
+
